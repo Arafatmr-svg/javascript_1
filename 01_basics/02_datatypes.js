5f3da898084643203036e770ef96ba2a7ede8906
Arafatmr-svg <mdr125742@gmail.com>
2024-08-29 15:48:47 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+console.log("arafat")
+let name="Arafat"
+let age=23;
+let isLoggedIn=true
+let state
+
+
+
+//number=>2 to power 53
+//bigint
+// string="''"
+//null=standalone value
+//undefined=>
+    //symbol=> unique
+
+
+
+//object
+
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof name)
+console.log(typeof null)// object
+console.log(typeof undefined)
