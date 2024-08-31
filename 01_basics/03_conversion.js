2ac382e5ad90c635534221d2810c771dfd2630ed
Arafatmr-svg <mdr125742@gmail.com>
2024-08-31 11:12:25 +0000

    learning

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,82 @@
+// let score = null
+// let score=undefined
+// let score=true
+let score="arafat"
+
+
+// console.log(typeof score)
+// console.log(typeof (score));
+
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33"=>33
+// "33abc"=>NaN
+// true=>1;false=>0
+
+let isLoggedIn="ggh"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+//  1= true
+//  0=false
+// ""=false
+// "chhj"=true
+
+let snumber=3
+let stringNumber= String(snumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// -------------------------------*operation---------------------------------------
+
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+// 2+2 
+// 2-2
+// 2*2
+// 2**2
+// 2/2
+// 2%3
+let str1="hello"
+let str2=" bangi"
+let str3= str1+str2
+// console.log(str3)
+
+// console.log("2"+ 1);
+// console.log(2+"1");
+// console.log("1" +2 +4);
+// console.log(1+2+"3");
+
+// console.log(+true)
+// console.log(+"");
+
+let gameP=244
+gameP++;
+// console.log(gameP);
+
+
+// let x=3;
+// let y=x++
+// console.log(x);
+// console.log(y);
+
+let x=3;
+let y=++x;
+console.log(x);
+console.log(y);
+
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2<=1)
+// console.log(2!=1)
+// console.log(2>1)
+
+// console.log("2">1);
+console.log("0"==false);
+
+// console.log("02">1);
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(null<=0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+// console.log(undefined<=0);
+
+
+//strict check
+// ===
+
+// console.log("22"==="22");
+// console.log("22"===222);
+// console.log(22===22);
+
+
+
