c49a15671fa22116ef4aebc73f0b1be789362780
Arafatmr-svg <mdr125742@gmail.com>
2024-09-04 23:09:32 +0000

    learning

---
+++ 01_basics/06_nums_&_maths.js
@@ -0,0 +1,44 @@
+const score=400
+
+const balance=new Number(100)
+// console.log(balance);
+// console.log(score);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber=233.4346
+// console.log(otherNumber.toPrecision(4));// in precision the value should be same amount of number before the dot  
+
+
+const hundreds =100000000
+// console.log(hundreds.toLocaleString("en-IN"));
+
+
+//---------------------------------------Maths---------------------------------------------------
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));// to change  negative to positive only
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(2.01));// more than any number would be the next number
+// console.log(Math.floor(3.9));// smaller number
+// console.log(Math.min(1,24,5,7,3,2,2));
+// console.log(Math.max(1,24,5,7,3,2,2));
+
+console.log(Math.random());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
