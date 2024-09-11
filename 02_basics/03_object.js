a96f4d13a8e737b133b2e410092ea8254d153668
Arafatmr-svg <mdr125742@gmail.com>
2024-09-11 11:39:00 +0000

    zd

---
+++ 02_basics/03_object.js
@@ -0,0 +1,43 @@
+//singleton
+//object.create
+
+
+//object literals
+
+
+const mySym=Symbol("key1")
+
+const JsUser={
+    name:"Arafat",
+    age:"23",
+    "full name":"Yeasin Arafat",
+    [mySym]:"mykey1",
+    location:"RAK",
+    email:"arafatmr@",
+    isLoggedIn:"false",
+    lastLogIn:["Monday","tuesday"]
+}
+
+// console.log(JsUser.age);
+// console.log(JsUser["age"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+
+// Object.freeze(JsUser) // to freeze the object
+
+JsUser.email="mdr12@"
+
+// console.log(JsUser);
+
+JsUser.greeting=function(){
+    console.log("hello js user");    
+}
+JsUser.greeting2=function(){
+    console.log(`hello js user,your email is ${this.email}`);    
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greeting2());
+
+
+
