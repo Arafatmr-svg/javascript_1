970cf999221f0b731a8a6579c68cb74d19474696
Arafatmr-svg <mdr125742@gmail.com>
2024-09-12 14:24:10 +0000

    s

---
+++ 02_basics/05_object_de_Structuring.js
@@ -0,0 +1,26 @@
+
+
+const course={
+
+    coursename:"js",
+    price:"3566",
+    instructor:"Arafat"
+
+}
+
+// console.log(course.coursename)
+// instructor="Yeasin"
+const {instructor:ins}=course
+// console.log(instructor);
+console.log(ins);
+
+
+
+//------------------------------------api----------------------------
+
+// {
+//     "name":"Ar",
+//     "course":"js",
+//     "price":"500"
+
+// }
