fa80f26db189afb5a68ff150bd720cb60f37a4cb
Arafatmr-svg <mdr125742@gmail.com>
2024-09-16 10:47:57 +0000

    jjh

--- 03_basics/05_IIFE.js
+++ 03_basics/05_IIFE.js
@@ -2,3 +2,17 @@
 
 //-----------------immediately invoked function expressions
 
+
+function func1(){
+    console.log("sfsf")
+    
+}
+// func1()
+
+
+(function func2(){
+    console.log(2334);
+    
+}
+
+)()
