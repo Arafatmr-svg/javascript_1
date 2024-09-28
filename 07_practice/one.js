275fc8cae768cc43238cde55cfaa45835e61f577
Arafatmr-svg <mdr125742@gmail.com>
2024-09-28 01:58:12 +0000

    jjh

--- 07_practice/one.js
+++ 07_practice/one.js
@@ -1,27 +1,81 @@
-const buttons=document.querySelectorAll('.button')
-console.log(buttons);
+// // const buttons=document.querySelectorAll('.button')
+// // // console.log(buttons);
 
-const body=document.querySelector("body")
-console.log(body);
-buttons.forEach(function(button){
-    console.log(button);
-    button.addEventListener("click",function(e){
-console.log(e);
-console.log(e.target);
-if(e.target.id=="box1"){
-body.style.backgroundColor="red"
-}
-if(e.target.id=="box2"){
-body.style.backgroundColor="blue"
-}
-if(e.target.id=="box3"){
-body.style.backgroundColor="gray"
-}
-if(e.target.id=="box4"){
-body.style.backgroundColor="green"
-}
+// // const body=document.querySelector("body")
+// // // console.log(body);
+// // buttons.forEach(function(button){
+// //     // console.log(button);
+// //     button.addEventListener("click",function(e){
+// // console.log(e);
+// // // console.log(e.target);
+// // if(this.id=="box1"){
+// // body.style.backgroundColor="red"
+// // }
+// // if(this.id=="box2"){
+// // body.style.backgroundColor="blue"
+// // }
+// // if(this.id=="box3"){
+// // body.style.backgroundColor="gray"
+// // }
+// // if(this.id=="box4"){
+// // body.style.backgroundColor="green"
+// // }
 
 
-    })
+// //     })
 
-})
+// // })
+
+
+
+// //
+
+
+
+// const buttons = document.querySelectorAll('.button');
+// const body = document.querySelector("body");
+
+// buttons.forEach(function(button) {
+//     button.addEventListener("click", function() {
+//         switch (this.id) {
+//             case "box1":
+//                 body.style.backgroundColor = "red";
+//                 break;
+//             case "box2":
+//                 body.style.backgroundColor = "blue";
+//                 break;
+//             case "box3":
+//                 body.style.backgroundColor = "gray";
+//                 break;
+//             case "box4":
+//                 body.style.backgroundColor = "green";
+//                 break;
+//         }
+//     });
+// });
+
+
+
+
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector("body");
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        console.log("Button clicked: " + this.id);
+        switch (this.id) {
+            case "box1":
+                body.style.backgroundColor = "red";
+                break;
+            case "box2":
+                body.style.backgroundColor = "blue";
+                break;
+            case "box3":
+                body.style.backgroundColor = "gray";
+                break;
+            case "box4":
+                body.style.backgroundColor = "green";
+                break;
+        }
+    });
+});
