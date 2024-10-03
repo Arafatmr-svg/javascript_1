bec442b14c76d8e3949a0e05060e158b6c4b41d2
Arafatmr-svg <mdr125742@gmail.com>
2024-10-03 15:31:08 +0000

    jj

--- 08_eventListener/four.js
+++ 08_eventListener/four.js
@@ -4,7 +4,51 @@ const randomColor=function(){
     const hex="0123456789ABCDEF"
     let color="#"
     for (let i = 0;i<6;i++) {
-       
+       color+=hex[Math.floor(Math.random()*16)]
     }
+    return color
 }
-console.log(Math.random()*16)
+let intervalId
+const startChangingColor=function(){
+    if(!intervalId){
+    console.log("Color changing started successfully");
+    intervalId=setInterval(changeBgColor,2000)
+    }
+    function changeBgColor(){
+       
+        
+        document.body.style.backgroundColor=randomColor()
+    }
+
+}
+
+const stopChangingColor=function(){
+    console.log("color changing stopped successfully");
+    
+clearInterval(intervalId)
+intervalId=null
+}
+
+ document.getElementById("start")
+ .addEventListener("click",startChangingColor)
+document.getElementById("stop")
+.addEventListener("click",stopChangingColor)
+
+
+// document.getElementById("start")
+// .addEventListener("click",function(){
+//     console.log("started successfully");
+    
+//       let stop=setInterval(function(){
+//         console.log("wsf");
+//         },2000)
+
+
+
+//         document.getElementById("stop")
+//         .addEventListener("click",function(){
+//             console.log("cleared successfully");
+            
+//             clearInterval(stop)
+//         })
+// })
